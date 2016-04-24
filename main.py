948fd202aa99b62d063dc722a1420f0f2445dffa
rajeevdesai <rajeevdesai94@gmail.com>
2016-04-24 15:54:52 +0530

    Initial Commit. Work needed on algorithm. GUI yet to be done

---
+++ main.py
@@ -0,0 +1,75 @@
+import random
+from PyDictionary import PyDictionary
+
+dictionary=PyDictionary()
+file=open("words.txt","r")
+words=file.readline().lower()
+used_words=[]
+new_words=[]
+new=""
+
+def generate_word_easy(word):
+    gen_word=list(word)
+    char='a'
+    for i in range(0,4):
+        for j in range(0,26):
+            gen_word[i]=chr(ord(char)+j)
+            new_word=''.join(gen_word)
+            #print(new_word)
+            if(new_word in words and new_word not in used_words):
+                used_words.append(new_word)
+                return new_word
+                break
+        gen_word[i]=word[i]
+    return "Word Doesn't Exist!"
+
+def generate_word_medium(word):
+    gen_word=list(word)
+    char='a'
+    for i in range(0,4):
+        for j in range(0,26):
+            gen_word[i]=chr(ord(char)+j)
+            new_word=''.join(gen_word)
+            #print(new_word)
+            if(new_word in words and new_word not in used_words):
+                new_words.append(new_word)
+        gen_word[i]=word[i]
+    index=random.randint(0,len(new_words))
+    if len(new_words)!=0:
+        used_words.append(new_words[index])
+        return new_words[index]
+    else:
+        return "Word Doesn't Exist!"
+
+def check_word_validity(word):
+    if(word in used_words):
+        print("Word is already used!")
+        return False
+    if(word not in words):
+        print("Word is not a valid word!")
+        return False
+    if(word == "Exit Game"):
+        return False
+    return True
+
+def check_new_word_validity(new):
+    new=generate_word_easy(new)
+    if(word not in new_words):
+        print("Invalid Word!")
+        return False
+    return True
+
+word=input("Player : ")
+new=word
+while(word != "Exit Game"):
+    del new_words
+    new_words=[]
+    if(new != "Word Doesn't Exist!" and check_word_validity(word) != False and check_new_word_validity(new) != False):
+        used_words.append(word)
+        new=generate_word_easy(word)
+        print("Computer : ",new)
+        word=input("Player : ")
+    else:
+        word=input("Player : ")
+
+print("Thank You for playing Addictionary!")
---
+++ view.py
@@ -0,0 +1,38 @@
+from tkinter import *
+
+myapp = Tk()
+
+
+def new():
+    top = toplevel()
+    print("new")
+
+
+myapp.wm_title("Addictionary")
+myapp.geometry("500x500")
+myapp.resizable(0, 0)
+myapp.config(background="#28ABE3")
+
+menubar = Menu(myapp)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New Game", command=new)
+filemenu.add_separator()
+filemenu.add_command(label="Close", command=myapp.quit)
+menubar.add_cascade(label="Game", menu=filemenu)
+myapp.config(menu=menubar)
+
+filename1 = PhotoImage(file="res/logo.png", width="361", height="71")
+logo = Label(myapp, image=filename1, bg="#28ABE3")
+logo.pack()
+
+new_btn = Button(myapp, relief=FLAT, bg="#28ABE3")
+filename2 = PhotoImage(file="res/new_game.png")
+new_btn.config(image=filename2, width="183", height="43")
+new_btn.pack()
+
+rul_btn = Button(myapp, relief=FLAT, bg="#28ABE3")
+filename3 = PhotoImage(file="res/rules.png")
+rul_btn.config(image=filename3, width="101", height="43")
+rul_btn.pack()
+
+myapp.mainloop()
